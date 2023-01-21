c93466731955a708ef6a30d218e3598e7192f072
hooni <jhlee909090@gmail.com>
2023-01-21 21:48:54 +0900

    .

--- appleMusic/ViewController.swift
+++ appleMusic/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func setupTableView() {
+        myTableView.dataSource = self
+        myTableView.delegate = self
+    }
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
 }
-//dsf
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
